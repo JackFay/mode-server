f8cda62ec02baf84733c3269067f04034cf9d8c0
Jack Fay <jack@tripleblind.ai>
2022-03-01 19:10:54 -0600

    First Commit

---
+++ devops-task/src/index.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>PHP Task</title>
+ </head>
+ <body>
+ <?php echo "<p>My PHP Test Task is Working. And yes, Hello World</p><br />" ;?>
+ <?php readfile("/var/www/html/version.env", "r") ;?>
+ </body>
+</html>
